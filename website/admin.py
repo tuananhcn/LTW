d568c3564e6a6cc2bb4ccfba510ed2d88853614b
tuananhcn <nguyenanhbee12@gmail.com>
2024-04-20 14:35:14 +0700

    Almost

--- website/admin.py
+++ website/admin.py
@@ -3,8 +3,26 @@ import sqlite3
 admin = Blueprint("admin", __name__)
 
 sqldbname = 'LAPTRINHWEB.db'
-
+def admin_required(f):
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session or 'quyen' not in session or session['quyen'] != 1:
+            flash("You must log in as an admin to access this page", "error")
+            return redirect(url_for('admin.login'))
+        return f(*args, **kwargs)
+    return decorated_function
+def get_obj_user(username,password):
+    result =[]
+    conn = sqlite3.connect(sqldbname)
+    cur = conn.cursor()
+    sqlcommand = "Select * from user where email =? and password = ?"
+    cur.execute(sqlcommand,(username,password))
+    obj_user = cur.fetchone()
+    if obj_user:
+        result = obj_user
+    conn.close()
+    return result;
 @admin.route('/')
+@admin_required
 def index():
   conn = sqlite3.connect(sqldbname)
   cursor = conn.cursor()
@@ -13,7 +31,30 @@ def index():
   storages = cursor.fetchall()
   conn.close()
   return render_template('admin/dashboard.html', storages=storages)
-
+@admin.route('/users')
+def Users():
+  conn = sqlite3.connect(sqldbname)
+  cursor = conn.cursor()
+  sqlcommand = ("SELECT * FROM user")
+  cursor.execute(sqlcommand)
+  storages = cursor.fetchall()
+  conn.close()
+  return render_template('admin/users.html', storages=storages)
+@admin.route('/users/add', methods=['GET','POST'])
+def addUser():
+  if request.method == 'POST':
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    quyen = request.form['quyen']
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO user (name, email, password, quyen) VALUES(?,?,?,?)", (name, email, password, quyen))
+    conn.commit()
+    conn.close()
+    flash("You have added successfully", "success")
+    return redirect(url_for('admin.Users'))
+  return render_template('admin/addUser.html')
 @admin.route('/add', methods=['GET','POST'])
 def add():
   if request.method == 'POST':
@@ -33,7 +74,6 @@ def add():
     flash("You have added successfully", "success")
     return redirect(url_for('admin.index'))
   return render_template('admin/add.html')
-
 @admin.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_product(id):
   conn = sqlite3.connect(sqldbname)
@@ -57,9 +97,26 @@ def edit_product(id):
     flash("Product updated successfully!", "success")
     return redirect(url_for("admin.index"))
   return render_template("admin/edit.html", storage=storage)
-
-
-
+@admin.route('/users/edit/<int:id>', methods=['GET', 'POST'])
+def edit_user(id):
+  conn = sqlite3.connect(sqldbname)
+  cursor = conn.cursor()
+  cursor.execute("SELECT * FROM user where user_id = ?", (id,))
+  storage = cursor.fetchone()
+  conn.close()
+  if request.method == 'POST':
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    quyen = request.form['quyen']
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+    cursor.execute("Update user set name = ?, email = ?, password = ?, quyen = ? where user_id = ?", (name, email, password, quyen, id))
+    conn.commit()
+    conn.close()
+    flash("Product updated successfully!", "success")
+    return redirect(url_for("admin.Users"))
+  return render_template("admin/editUser.html", storage=storage)
 @admin.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
   conn = sqlite3.connect(sqldbname)
@@ -69,3 +126,31 @@ def delete(id):
   conn.close()
   flash("You have successfully deleted the item", "success")
   return redirect(url_for('admin.index'))
+@admin.route('/users/delete/<int:id>', methods=['POST'])
+def deleteUser(id):
+  conn = sqlite3.connect(sqldbname)
+  cursor = conn.cursor()
+  cursor.execute("DELETE FROM user WHERE user_id = ?", (id,))
+  conn.commit()
+  conn.close()
+  flash("You have successfully deleted user", "success")
+  return redirect(url_for('admin.Users'))
+@admin.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['txt_username']
+        password = request.form['txt_password']
+        obj_user = get_obj_user(username,password)
+        if obj_user:
+            obj_user ={
+                "id" : obj_user[0],
+                "name": obj_user[1],
+                "email":obj_user[2],
+                "quyen": obj_user[3]
+            }
+            session['current_user'] = obj_user
+            flash("You have log in successfully", "success")
+            return redirect(url_for('admin.index'))
+        flash("Please check your username and password", 'error')
+        return redirect(url_for('admin.login'))
+    return render_template('login.html')
